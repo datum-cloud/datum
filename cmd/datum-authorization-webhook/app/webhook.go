e18be36581fe4b17e0ce0360b4c6827b62a9f082
Scot Wells <wells.scot@gmail.com>
2024-12-13 21:34:50 +0000

    chore: align binary name with service name

--- cmd/datum-authorization-webhook/app/webhook.go
+++ cmd/datum-authorization-webhook/app/webhook.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 func NewWebhook() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "authorization-webhook",
+		Use:   "datum-authorization-webhook",
 		Short: "An authorization webhook backed by the Datum IAM service",
 	}
 
